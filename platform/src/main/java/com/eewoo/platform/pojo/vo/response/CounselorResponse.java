b50a31fd28ae4ff28571e8cac6073b229587b9da
Lookingatyouc <clbwc@outlook.com>
2023-05-31 09:09:52 +0800

    Couselor, spervisor部分接口完成

--- platform/src/main/java/com/eewoo/platform/pojo/vo/response/CounselorResponse.java
+++ platform/src/main/java/com/eewoo/platform/pojo/vo/response/CounselorResponse.java
@@ -1,25 +1,12 @@
 package com.eewoo.platform.pojo.vo.response;
 
-
-import lombok.Data;
-
-@Data
 public class CounselorResponse {
     Integer id;
     String username;
-    Integer banned;
     String name;
+    String banned;
     String profile;
-    Integer consultDurationTotal;
-    Integer consultCntTotal;
-    Integer consultScoreTotal;
-    Integer consultCntToday;
-    Integer consultDurationToday;
-    Integer age;
-    String idCard;
     String phone;
     String email;
-    String workPlace;
-    String title;
-}
 
+}
